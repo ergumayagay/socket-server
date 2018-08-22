28c98b48c1203e91a766b1bae84e7048099ae4a1
Emilio Ramon GUMAYAGAY <emilio.gumayagay@tap-group.com.cn>
2018-08-22 09:33:11 +0800

    initial commit

---
+++ src/main/java/org/tap/socketserver/SocketServerApplication.java
@@ -0,0 +1,37 @@
+package org.tap.socketserver;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootApplication
+public class SocketServerApplication {
+	
+	
+	
+	private static final Logger logger = (Logger) LoggerFactory.getLogger(SocketServerApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketServerApplication.class, args);
+	}
+	
+
+	@Bean
+	public CommandLineRunner run(SocketServerProperties socketProperties, 
+			SocketServerService socketServerService) {
+		return args ->{
+			
+			socketServerService.start();
+			logger.info(Integer.toString(socketProperties.getPort()));		
+			
+			
+		};
+		
+	}
+	
+	
+}
